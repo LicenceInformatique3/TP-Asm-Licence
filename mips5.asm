ce1524e52561db9c47b3effea62d180f61ec792b
moonlight83340 <moonlight83340@users.noreply.github.com>
2017-02-21 23:38:53 +0100

    Update mips5.asm

--- mips5.asm
+++ mips5.asm
@@ -1,45 +1,42 @@
+.data
+str1: .asciiz "Entrez un entier : "
+str2: .asciiz "Son factoriel est "
+ret:  .asciiz "\n" 
  .text
- main:
- # Print msg1
- li $v0,4 # print_string syscall code = 4
- la $a0, msg1 
- syscall
- #
- # Get N from user and save
- li $v0,5 # read_int syscall code = 5
- syscall 
- move $t0,$v0 # syscall results returned in $v0 = n
- #
- # Initialize registers
- li $t1, 1 # initialize res
- li $t2,1 # constante 1
- #
-  # Main loop body
- fact_tail:
- beq $t0,$t2,exit
- mul $t1,$t1,$t0
- sub $t0,$t0,$t2
- j fact_tail
- # Exit routine - print msg2
- exit:
- li $v0, 4 # print_string syscall code = 4
- la $a0, msg2
- syscall
- #
- # Print sum
- li $v0,1 # print_string syscall code = 4
- move $a0, $t1
- syscall
- #
- # Print newline
- li $v0,4 # print_string syscall code = 4
- la $a0, lf
- syscall
- li $v0,10 # exit
- syscall
- #
- # Start .data segment (data!)
- .data
- msg1: .asciiz "Number of integers (N)? "
- msg2: .asciiz "Facto = "
- lf: .asciiz "\n"
+
+fact: 
+       li $v0, 4
+       la $a0, str1
+       syscall                                           # demander un entier
+ 
+       li $v0, 5
+       syscall                                           # mettre l'entier dans $v0
+
+       move $a0, $v0                                     # prepare le paramètre pour l'appel
+       li $a1, 1
+       jal fact_tail                                     # appel fact, resultat dans $3
+  
+       li $v0, 4
+       la $a0, str2 
+       syscall                                           # annonce resultat 
+ 
+       li $v0, 1
+       move $a0 $3
+       syscall                                           # affiche resultat
+ 
+       li $v0, 4
+       la $a0, ret
+       syscall                                           # retour à la ligne
+ 
+       li $v0, 10
+       syscall                                           # on sort du prog (exit)
+
+
+fact_tail:
+       bgt $a0 1 recur                                   # si n<>1 aller en recur 
+       move $3, $a1                                      # sinon resultat=res
+       jr $ra                                            # return
+    recur:
+       mul $a1 $a0 $a1                                   # res=res*n
+       sub $a0 $a0 1                                     # n=n-1
+       j fact_tail
